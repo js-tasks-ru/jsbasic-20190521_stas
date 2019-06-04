0a524f16d050236684ac25bec3e86961e2f94d49
Stanislav Gedmin <s.gedmin@gmail.com>
2019-06-04 12:26:48 +0300

    module2 задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,24 @@
+'use strict'
 /**
  * Клонируем объект
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
 function clone (obj) {
+    let clone = {};
+    for (let key in obj){
+        if (typeof obj[key] == "object") {
+        clone[key] = {};
+        for (let key2 in obj[key]) {
+            if (typeof obj[key][key2] == "object"){
+            clone[key][key2] = {};
+            for (let key3 in obj[key][key2])
+                clone[key][key2][key3] = obj[key][key2][key3];
+            }
+            else clone[key][key2] = obj[key][key2];
+        }
+        }
+        else clone[key] = obj[key];    
+    }
+  return (clone);
 }
